52639edb8aeedf18cf1446be05501f202b752fa8
revakovsky <revakovsky@gmail.com>
2024-04-19 15:02:13 +0300

    add to the custom view a TicTacToeField variable and its listener

--- app/src/main/java/com/revakovskyi/customview_tictactoe/TicTacToeView.kt
+++ app/src/main/java/com/revakovskyi/customview_tictactoe/TicTacToeView.kt
@@ -13,28 +13,57 @@ class TicTacToeView(
     defStyleRes: Int,
 ) : View(context, attributeSet, defStyleAttr, defStyleRes) {
 
-    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : this(
-        context,
-        attributeSet,
-        defStyleAttr,
-        // Set a default style for the view if the Global attrStyle (in our case it is a "ticTacToeFieldStyle") wasn't set up
-        R.style.DefaultTicTacToeFieldStyle
-    )
-    constructor(context: Context, attributeSet: AttributeSet?) : this(
-        context,
-        attributeSet,
-        // Set a global style for the custom view in the current app
-        R.attr.ticTacToeFieldStyle
-    )
+
+    // Set a default style for the view if the Global attrStyle (in our case it is a "ticTacToeFieldStyle") wasn't set up
+    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : this(context, attributeSet, defStyleAttr, R.style.DefaultTicTacToeFieldStyle)
+
+    // Set a global style for the custom view in the current app
+    constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, R.attr.ticTacToeFieldStyle)
+
+    // Set a constructor to create a view from the code with minimal params
     constructor(context: Context) : this(context, null)
+
 
     private var player1Color by Delegates.notNull<Int>()
     private var player2Color by Delegates.notNull<Int>()
     private var gridColor by Delegates.notNull<Int>()
 
+    /**
+     * field?.listeners?.remove(listener) - unsubscribe the listener from the old field to prevent leakMemory
+     * Set to the ticTacToeField variable a new value - "value" and redraw the TicTacToeField by using the
+     * @see invalidate()
+     * field?.listeners?.add(listener) - subscribe a new listener to the new field
+     * If we want to change the size of the field on the layout: e.g. the field was the
+     * size of 3x4 and we wanna make it 5x7
+     * @see requestLayout()
+     */
+    private var ticTacToeField: TicTacToeField? = null
+        set(value) {
+            field?.listeners?.remove(listener)
+            field = value
+            field?.listeners?.add(listener)
+            requestLayout()
+            invalidate()
+        }
+
+    private val listener: OnFieldChangeListener = {
+
+    }
+
+
     init {
         if (attributeSet != null) initAttributes(attributeSet, defStyleAttr, defStyleRes)
         else initDefaultColors()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        ticTacToeField?.listeners?.add(listener)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        ticTacToeField?.listeners?.remove(listener)
     }
 
     private fun initAttributes(attributeSet: AttributeSet, defStyleAttr: Int, defStyleRes: Int) {
